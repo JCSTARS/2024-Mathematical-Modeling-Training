42eb04b18db795b45389450313678b545ff9e65c
09J22111姚泓宇 <168245315+Fitful-Nightmare@users.noreply.github.com>
2024-06-28 01:29:33 +0800

    Update ques_3_randomForest.py

--- ques_3_randomForest.py
+++ ques_3_randomForest.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-
+from sklearn.metrics import roc_auc_score
 def is_qualified(x):
     return 77.78 < x[1] < 80.33 and x[2] < 24.15 and x[3] < 17.15 and x[4] < 15.62
 
@@ -74,9 +74,53 @@ def is_qualified_single_column(preds, index_A_col, index_B_col, index_C_col, ind
     return (77.78 < preds[index_A_col] < 80.33) and (preds[index_B_col] < 24.15) and (preds[index_C_col] < 17.15) and (preds[index_D_col] < 15.62)
 
 # 应用函数到 Predqua_total 的每一行
-Predqua_total['合格'] = Predqua_total.apply(is_qualified_single_column, 
-                                            args=(['指标A (index A)_预测值', '指标B (index B)_预测值', '指标C (index C)_预测值', '指标D (index D)_预测值']), 
+Predqua_total['合格'] = Predqua_total.apply(is_qualified_single_column,
+                                            args=(['指标A (index A)_预测值', '指标B (index B)_预测值', '指标C (index C)_预测值', '指标D (index D)_预测值']),
                                             axis=1)
 
-print(Predqua_total['合格'])
-Predqua_total['合格'].to_excel("haha.xlsx")
+#print(Predqua_total['合格'])
+#Predqua_total['合格'].to_excel("haha.xlsx")
+#print('AUC:', roc_auc_score(datas['是否合格'],Predqua_total['合格'] ))
+
+testA1=[341.40,665.04,52.88,91.27,47.22,22.26,470.31]
+testA2=[341.40,665.04,52.88,91.27,47.22,22.26,479.71]
+testA3=[341.40,665.04,52.88,91.27,47.22,22.26,470.04]
+testA4=[341.40,665.04,52.88,91.27,47.22,22.26,444.46]
+testA5=[341.40,665.04,52.88,91.27,47.22,22.26,444.67]
+testA6=[341.40,665.04,52.88,91.27,47.22,22.26,449.67]
+testA7=[341.40,665.04,52.88,91.27,47.22,22.26,457.1]
+testA8=[341.40,665.04,52.88,91.27,47.22,22.26,437.68]
+testB1=[1010.32,874.47,54.44,92.12,48.85,21.83,453.56]
+testB2=[1010.32,874.47,54.44,92.12,48.85,21.83,450.9]
+testB3=[1010.32,874.47,54.44,92.12,48.85,21.83,437.04]
+testB4=[1010.32,874.47,54.44,92.12,48.85,21.83,426.66]
+testB5=[1010.32,874.47,54.44,92.12,48.85,21.83,421.47]
+testB6=[1010.32,874.47,54.44,92.12,48.85,21.83,433.41]
+testB7=[1010.32,874.47,54.44,92.12,48.85,21.83,403.17]
+testB8=[1010.32,874.47,54.44,92.12,48.85,21.83,450.13]
+
+testA1_array = np.array(testA1)
+testA2_array = np.array(testA2)
+testA3_array = np.array(testA3)
+testA4_array = np.array(testA4)
+testA5_array = np.array(testA5)
+testA6_array = np.array(testA6)
+testA7_array = np.array(testA7)
+testA8_array = np.array(testA8)
+testB1_array = np.array(testB1)
+testB2_array = np.array(testB2)
+testB3_array = np.array(testB3)
+testB4_array = np.array(testB4)
+testB5_array = np.array(testB5)
+testB6_array = np.array(testB6)
+testB7_array = np.array(testB7)
+testB8_array = np.array(testB8)
+
+TA1A_Pred = RFR_1.predict([testB8_array])
+TA1B_Pred = RFR_2.predict([testB8_array])
+TA1C_Pred = RFR_3.predict([testB8_array])
+TA1D_Pred = RFR_4.predict([testB8_array])
+print(TA1A_Pred)
+print(TA1B_Pred)
+print(TA1C_Pred)
+print(TA1D_Pred)
