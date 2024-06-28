3f8f74a54a3898336b2afbed6af43790390b9abc
JCSTARS <143331086+JCSTARS@users.noreply.github.com>
2024-06-28 12:43:05 +0800

    更新了问题4的数据预处理

--- problem3_predata.py
+++ problem3_predata.py
@@ -35,7 +35,7 @@ exp_date1 = [
 cond1 = sheet1_time_string.apply(lambda x: x in exp_date1)
 data_part1 = data_part1[cond1.apply(lambda x: not x)]
 data_part1.index = [i for i in range(len(data_part1))]
-print(data_part1.shape)
+# print(data_part1.shape)
 
 # 找到有效的产品质量数据
 sheet2_time_string = sheet2.iloc[:, 0].astype('string')
@@ -50,7 +50,7 @@ exp_date2 = [
 cond2 = sheet2_time_string.apply(lambda x: x in exp_date2)
 data_part2_ = sheet2[cond2.apply(lambda x: not x)].iloc[2:, :]
 data_part2_.index = [i for i in range(len(data_part2_))]
-print(data_part2_.shape)
+# print(data_part2_.shape)
 
 # todo 计算合格数量、合格率
 cond10 = 77.78 < data_part2_.iloc[:, 1]
@@ -65,11 +65,12 @@ def is_qualified(x):
     return 77.78 < x[1] < 80.33 and x[2] < 24.15 and x[3] < 17.15 and x[4] < 15.62
 data_part2 = pd.DataFrame(data_part2_.apply(is_qualified, axis=1))
 data_part2.columns = ['是否合格']
+
 # print(len(data_part2))
 # print(data_part2.sum() / len(data_part2))
 
-print(data_part2.shape)
-print(data_part2.sum(), data_part2.sum() / len(data_part2))
+# print(data_part2.shape)
+# print(data_part2.sum(), data_part2.sum() / len(data_part2))
 
 # todo 找到原矿参数数据 3
 cnt = data_part1.iloc[:, 0].astype('string').apply(lambda x: x[5: 10])
@@ -84,19 +85,17 @@ print(data_part3.shape)
 cols = ['时间 (Time)', "过程数据3 (Process parameter 3)", "过程数据4 (Process parameter 4)"]
 proc_data = pd.DataFrame(sheet4)
 #proc_data.iplot(x='时间 (Time)')
-#print("相关系数：")
-proc_data.iloc[:, 1:].corr()
+print("相关系数：", proc_data.iloc[:, 1:].corr())
+
 
 def norm(data):
     return (data - data.min()) / (data.max() - data.min())
 
 data_part4 = sheet4.apply(lambda x: (x[3] + x[4]), axis=1)
 data_part4 = pd.concat([sheet4.iloc[:, 0], data_part4], axis=1).rename(columns={0: "原矿质量"})
-print(data_part4.shape)
+# print(data_part4.shape)
 # data_part4
 # mitosheet.sheet(data_part4, analysis_to_replay="id-lntnexsmmk")
-
-
 
 exp_date4 = []
 for i in exp_date1 + exp_date2:
@@ -114,11 +113,14 @@ for _ in range(5):
 data_part4_need.index = [i for i in range(len(data_part4_need))]
 
 data_part4_need = pd.DataFrame(np.repeat(data_part4_need.values, 3, axis=0), columns=data_part4_need.columns)
-print(data_part4_need.shape)
+
+proc_data = proc_data.iloc[:,3:5]
+data_proc_need = pd.DataFrame(np.repeat(proc_data.values, 3, axis=0), columns=proc_data.columns)
+# print(data_part4_need.shape)
 # data_part4_need
 # mitosheet.sheet(data_part4_need, analysis_to_replay="id-owygulbcev")
 
-combined_data = pd.concat([data_part1, data_part2, data_part3, data_part4], axis=1)
+combined_data = pd.concat([data_part1, data_part2_, data_part2, data_part3, data_part4_need, data_proc_need], axis=1)
 combined_data.to_excel('attachment2.xlsx', sheet_name='Sheet1', index=False)
 
 X = pd.concat([data_part1.iloc[:, 1:], data_part3.iloc[:, 1:], data_part4_need.iloc[:, 1:]], axis=1)
@@ -132,5 +134,60 @@ remain_index = X[cond].index
 
 X = X[cond]
 Y = Ys[cond].replace(to_replace=[True, False], value=[1, 0])
-print(X.shape, Y.shape)
+# print(X.shape, Y.shape)
+ 
+datas =  data_part2_
+# 假设时间列是第一列
+datas['日期 (Date)'] = datas.iloc[:, 0].dt.date
 
+# 应用is_qualified函数到DataFrame的行，并创建一个新列'是否合格'
+datas['是否合格'] = datas.apply(is_qualified, axis = 1)
+
+# 按日期分组并计算每天的合格率
+daily_qualified_counts = datas.groupby('日期 (Date)')['是否合格'].sum()
+daily_total_counts = datas.groupby('日期 (Date)')['是否合格'].count()
+daily_pass_rate = (daily_qualified_counts / daily_total_counts) * 100
+
+# 创建一个包含日期和合格率的DataFrame
+daily_pass_rate_df = pd.DataFrame({'日期': daily_pass_rate.index, '合格率': daily_pass_rate})
+
+datas =  data_part1
+# 假设时间列是第一列
+datas['日期 (Date)'] = datas.iloc[:, 0].dt.date
+# print(datas)
+# 按日期分组并计算每天的合格率
+daily_tema1 = datas.groupby('日期 (Date)')['系统I温度 (Temperature of system I)'].mean()
+daily_tema2 = datas.groupby('日期 (Date)')['系统II温度 (Temperature of system II)'].mean()
+daily_temv1 = datas.groupby('日期 (Date)')['系统I温度 (Temperature of system I)'].var()
+daily_temv2 = datas.groupby('日期 (Date)')['系统II温度 (Temperature of system II)'].var()
+# 创建一个包含日期和合格率的DataFrame
+daily_temp = pd.DataFrame({'温度1均值': daily_tema1,'温度2均值': daily_tema2,
+                           '温度1方差': daily_temv1,'温度2方差': daily_temv2})
+# print(daily_temp)
+
+datas =  data_part3
+# 假设时间列是第一列
+datas['日期 (Date)'] = datas.iloc[:, 0].dt.date
+
+daily_mine = pd.DataFrame({'原矿参数1': datas.groupby('日期 (Date)')['原矿参数1 (Mineral parameter 1)'].mean(),
+                           '原矿参数2': datas.groupby('日期 (Date)')['原矿参数2 (Mineral parameter 2)'].mean(),
+                           '原矿参数3': datas.groupby('日期 (Date)')['原矿参数3 (Mineral parameter 3)'].mean(),
+                           '原矿参数4': datas.groupby('日期 (Date)')['原矿参数4 (Mineral parameter 4)'].mean()})
+
+print(daily_mine)
+
+datas = data_part4_need
+# 假设时间列是第一列
+datas['日期 (Date)'] = datas.iloc[:, 0].dt.date
+
+# print(datas)
+# 按日期分组并计算每天的合格率
+daily_quaa = datas.groupby('日期 (Date)')['原矿质量'].mean()
+daily_quav = datas.groupby('日期 (Date)')['原矿质量'].var()
+# 创建一个包含日期和合格率的DataFrame
+daily_qua = pd.DataFrame({'原矿质量均值': daily_quaa,'原矿质量方差': daily_quav})
+print(daily_qua)
+
+combined_data = pd.concat([daily_pass_rate_df, daily_temp, daily_mine, daily_qua], axis=1)
+combined_data.dropna(how='any', inplace=True)
+combined_data.to_excel('question4.xlsx', sheet_name='Sheet1', index=False)
